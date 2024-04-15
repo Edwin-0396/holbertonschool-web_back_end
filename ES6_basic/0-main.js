d0730ab90f731c0a1828ae8aee3766ef91d034a9
Edwin-0396 <egc4611@gmail.com>
2024-04-15 11:58:52 -0500

    Adding new files

---
+++ ES6_basic/0-main.js
@@ -0,0 +1,7 @@
+// Instead of import { taskFirst, taskNext } from './0-constants.js';
+const { taskFirst, taskNext } = require('./0-constants.js');
+
+// Your code here
+
+
+console.log(`${taskFirst()} ${taskNext()}`);
