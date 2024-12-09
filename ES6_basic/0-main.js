51018e146b75a236556228e577cf123ef815a9b9
Developer.CQ <developer.cq.colombia@gmail.com>
2024-12-09 15:31:28 -0500

    new Project ES6_basic

--- ES6_basic/0-main.js
+++
@@ -1,7 +0,0 @@
-// Instead of import { taskFirst, taskNext } from './0-constants.js';
-const { taskFirst, taskNext } = require('./0-constants.js');
-
-// Your code here
-
-
-console.log(`${taskFirst()} ${taskNext()}`);
